6fd7b5330b7d70f3b3320c8f9ebd27cfb37720f9
Mac10 <mac10@iMac-de-Mac10.local>
2021-10-20 16:30:32 -0500

    Antes-Revisar

---
+++ Practica_Examen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica_Examen
+//
+//  Created by Mac10 on 20/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
