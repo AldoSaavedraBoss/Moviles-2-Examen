6742b2d8acdefabbe2320b44b2e6595f6f41f9ee
Mac10 <mac10@iMac-de-Mac10.local>
2021-10-20 21:00:58 -0500

    4 pantallas listas

---
+++ Practica_Examen/UsuarioViewController.swift
@@ -0,0 +1,33 @@
+//
+//  UsuarioViewController.swift
+//  Practica_Examen
+//
+//  Created by Mac10 on 20/10/21.
+//
+
+import UIKit
+
+class UsuarioViewController: UIViewController {
+
+    @IBOutlet weak var LBNombre: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func RecibirDatos(_sender: UIStoryboardSegue){
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Practica_Examen/ViewController.swift
+++ Practica_Examen/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TFPass: UITextField!
+    @IBOutlet weak var TFUsuario: UITextField!
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let usuario = TFUsuario.text ?? "Anonimo"
+        let  destino = segue.destination as! UsuarioViewController
+        destino.LBNombre.text = usuario
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
